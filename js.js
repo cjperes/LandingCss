4bc12155eb1c3cec3908db3808b86dc33e9c6b4d
cjperes <caio@bbcreative.org>
2017-05-09 10:55:12 -0300

    Att
    Add smoth scroll with JS
    Add Pure css parallax

---
+++ js.js
@@ -0,0 +1,9 @@
+
+// jQuery Smoth Scroll
+$('#up, #down').on('click', function(e){
+    e.preventDefault();
+    var target= $(this).get(0).id == 'up' ? $('#down') : $('#up');
+    $('html, body').stop().animate({
+       scrollTop: target.offset().top
+    }, 1000);
+});
